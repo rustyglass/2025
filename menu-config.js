52cc06db2d54d922e99276a562c0fcce4ad638ab
rustyglass <rusty.glass@icloud.com>
2025-08-16 10:04:41 -0500

    Create menu-config.js

---
+++ menu-config.js
@@ -0,0 +1,15 @@
+const menuItems = [
+  { name: "Home", link: "index.html" },
+  { name: "Prompts", link: "prompts.html" },
+  { name: "Newsfeed", link: "newsfeed.html" },
+  { name: "Submissions", link: "submissions.html" },
+  {
+    name: "Other Challenges",
+    submenu: [
+      { name: "2025 Challenge", link: "https://2025.readingchallenge.online" },
+      { name: "2025 Summer", link: "https://summer.2025.readingchallenge.online" },
+      { name: "Main Website", link: "https://www.readingchallenge.online" },
+    ]
+  },
+  { name: "Contact", link: "contact.html" }
+];
