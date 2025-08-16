c6489585ad674d352b6a1cfcf919686193924e6a
rustyglass <rusty.glass@icloud.com>
2025-08-16 10:05:45 -0500

    Create newsfeed.js

---
+++ newsfeed.js
@@ -0,0 +1,18 @@
+// Put newest at the top OR add a `date` field to let the page auto-sort.
+// date can be "2025-08-01 10:30:00" or "2025-08-01T10:30:00"
+window.NEWS_POSTS = [
+  {
+    title: "Welcome to the 2026 Reading Challenge!",
+    body: "Make sure to take a look at the Prompts page and make sure your prompts are entered correctly!",
+    button: "View Prompts",
+    link: "prompts.html",
+    date: "2025-08-12 21:30:00"
+  },
+  {
+    title: "New Submissions Page",
+    body: "View the new submissions page, which lets you see and filter all 2026 Reading Challenge Submissions!",
+    button: "Open Submissions",
+    link: "submissions.html",
+    date: "2025-08-13 18:00:00"
+  },
+];
