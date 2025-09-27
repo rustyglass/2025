159927b017781c85f5cdff1327937a3d9431b8c2
rustyglass <rusty.glass@icloud.com>
2025-09-27 14:33:31 -0500

    Update newsfeed.js

--- newsfeed.js
+++ newsfeed.js
@@ -2,17 +2,17 @@
 // date can be "2025-08-01 10:30:00" or "2025-08-01T10:30:00"
 window.NEWS_POSTS = [
   {
-    title: "Welcome to the 2026 Reading Challenge!",
-    body: "Make sure to take a look at the Prompts page and make sure your prompts are entered correctly!",
-    button: "View Prompts",
-    link: "prompts.html",
-    date: "2025-08-12 21:30:00"
+    title: "Welcome to the new 2025 Reading Challenge Website!",
+    body: "We've redesigned the entire website to match the theme of the 2026 Site! Check it out!",
+    button: "",
+    link: "",
+    date: "2025-09-27 14:45:00"
   },
   {
     title: "New Submissions Page",
-    body: "View the new submissions page, which lets you see and filter all 2026 Reading Challenge Submissions!",
+    body: "View the new submissions page, which lets you see and filter all 2025 Reading Challenge Submissions!",
     button: "Open Submissions",
     link: "submissions.html",
-    date: "2025-08-13 18:00:00"
+    date: "2025-09-27 14:00:00"
   },
 ];
