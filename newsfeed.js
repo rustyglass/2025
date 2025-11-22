ba6e08853d33c6f3a9f5178138b41123ed0c3e68
rustyglass <rusty.glass@icloud.com>
2025-11-21 18:59:03 -0600

    Update news posts in newsfeed.js

--- newsfeed.js
+++ newsfeed.js
@@ -2,11 +2,11 @@
 // date can be "2025-08-01 10:30:00" or "2025-08-01T10:30:00"
 window.NEWS_POSTS = [
   {
-    title: "Submissions Page Issue Fixed",
-    body: "An issue with the Submissions page has been fixed: Some submissions are missing timestamps and being sorted incorrectly.",
+    title: "Cindy Wins 1st Place",
+    body: "Cindy is first to successfully complete all 85 prompts!",
     button: "",
     link: "",
-    date: "2025-10-04 13:14:00"
+    date: "2025-11-19 21:28:00"
   },
   {
     title: "Welcome to the new 2025 Reading Challenge Website!",
@@ -14,12 +14,5 @@ window.NEWS_POSTS = [
     button: "",
     link: "",
     date: "2025-09-27 14:45:00"
-  },
-  {
-    title: "New Submissions Page",
-    body: "View the new submissions page, which lets you see and filter all 2025 Reading Challenge Submissions!",
-    button: "Open Submissions",
-    link: "submissions.html",
-    date: "2025-09-27 14:00:00"
   },
 ];
